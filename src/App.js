7f4ded7109790afd9aee7aca371a345387778619
ziyadrahman <ziyad.rahman@icloud.com>
2024-10-01 08:29:48 +0530

    some changes in App.js and RowPost

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Banner from './Components/Banner/Banner';
 import Navbar from './Components/Navbar/Navbar';
+import RowPost from './Components/RowPost/RowPost';
 
 function App() {
   return (
     <div className="App">
     <Navbar/>
     <Banner/>
+    <RowPost/>
     </div>
   );
 }
