07424fa0e3f8ab105b9b4de150c4e8c391d02502
ziyadrahman <ziyad.rahman@icloud.com>
2024-10-19 17:26:32 +0530

    project-from-scratch

--- src/App.js
+++ src/App.js
@@ -1,16 +1,12 @@
-import './App.css';
-import Banner from './Components/Banner/Banner';
+import React from 'react';
 import Navbar from './Components/Navbar/Navbar';
-import RowPost from './Components/RowPost/RowPost';
+
 
 function App() {
   return (
-    <div className="App">
-    <Navbar/>
-    <Banner/>
-    <RowPost/>
-   
-    </div>
+<div className="App">
+  <Navbar/>
+</div>
   );
 }
 
--- src/Constants/Constants.js
+++
@@ -1,5 +0,0 @@
-// constants.js
-export const base_url = 'https://image.tmdb.org/t/p/';
-export const banner_size = 'original'; // For high-resolution banner images
-export const poster_size = 'w500'; // For RowPost images
-export const api_key = 'c85caea0a36ba0e0499c4e70b6f98e0e';
